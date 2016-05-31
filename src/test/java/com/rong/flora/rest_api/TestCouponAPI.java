257eccb252315b2279d4b7909279171d3b57934f
flora <bluerwf@gmail.com>
2016-05-31 21:50:14 +0800

    Add api test

--- src/test/java/com/rong/flora/rest_api/TestCouponAPI.java
+++ src/test/java/com/rong/flora/rest_api/TestCouponAPI.java
@@ -18,14 +18,42 @@ public class TestCouponAPI {
     @Test(parameters = {"couponId", "account", "operator"} )
     public void testSendCouponToTargetAccount(Integer couponId, Integer account, Integer operator) {
 
-        String URI = host + ":" + port+ uri + "/" + couponId + "/code/single" +"?account=" +account;
+        String URI = host + ":" + port+ uri + "/" + couponId + "/code/single";
         try {
-            HttpResponse<JsonNode> jsonResponse = Unirest.get(URI).asJson();
+            HttpResponse<JsonNode> jsonResponse = Unirest.get(URI)
+                    .queryString("account", account)
+                    .queryString("operator", operator)
+                    .asJson();
             Assert.assertEquals(jsonResponse.getStatus(),200,"ok");
+            Integer couponId1 = jsonResponse.getBody().getObject().getJSONObject("coupon").getInt("couponId");
+            Integer account1 = jsonResponse.getBody().getObject().getInt("account");
+            Assert.assertEquals(couponId1,couponId, "ok");
+            Assert.assertEquals(account1, account, "ok");
+            System.out.println(jsonResponse.getBody().toString());
         } catch (UnirestException e) {
             System.out.println(e.getMessage());
         }
+    }
 
+    @Test(parameters = {"invalidCouponId", "account", "operator"})
+    public void testNoCouponAvailable(Integer couponId, Integer account, Integer operator){
+        String URI = host + ":" + port + uri + "/" + couponId + "/code/single";
+        try {
+            HttpResponse<JsonNode> httpResponse = Unirest.get(URI)
+                    .queryString("account", account)
+                    .queryString("operator", operator)
+                    .asJson();
+            Integer statusCode = httpResponse.getStatus();
+            Integer error = httpResponse.getBody().getObject().getInt("error");
+            String message = httpResponse.getBody().getObject().getString("message");
+            Assert.assertEquals(statusCode, Integer.valueOf(200), "ok");
+            Assert.assertEquals(error, Integer.valueOf(6010), "ok");
+            Assert.assertEquals(message, "NO_COUPON_AVAILABLE", "ok");
+            System.out.println(httpResponse.getBody().toString());
 
+        }catch (UnirestException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
+
