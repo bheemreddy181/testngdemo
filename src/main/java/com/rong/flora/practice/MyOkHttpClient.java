e37e7054fd18fb8d702c075a47b8533a8154835f
flora <bluerwf@gmail.com>
2016-07-07 21:58:30 +0800

    Add statistics

---
+++ src/main/java/com/rong/flora/practice/MyOkHttpClient.java
@@ -0,0 +1,53 @@
+package com.rong.flora.practice;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import javax.net.ssl.*;
+
+/**
+ * Created by flora on 16/7/7.
+ */
+public class MyOkHttpClient {
+
+    private  static OkHttpClient client;
+
+    public static OkHttpClient getClient() {
+        try {
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[] {
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                        }
+
+                        @Override
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                            return new java.security.cert.X509Certificate[]{};
+                        }
+                    }
+            };
+
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+
+            client = new OkHttpClient();
+            client.setSslSocketFactory(sslSocketFactory);
+            client.setHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+            return client;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/com/rong/flora/practice/Statistics.java
+++ src/main/java/com/rong/flora/practice/Statistics.java
@@ -1,7 +1,11 @@
 package com.rong.flora.practice;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -37,28 +41,70 @@ public class Statistics {
         return map;
     }
 
+    public void sendPoints(String nm, Integer p, String gender){
+
+        Statistics.userSet.forEach(user -> {
+            switch (user.getGender()) {
+                case "f":
+                    break;
+                case "m":
+                    break;
+            }
+            if(user.getName().contains(nm)) {
+                if(user.getGender().equals("f")){
+                    user.addPoint(p);
+                }
+                user.addPoint(p);
+            }});
+
+//        Statistics.userSet
+//                .stream()
+//                .filter(user -> user.getName().contains(nm))
+//                .forEach(user -> user.addPoint(p));
+
+    }
+
 
     public static void main(String[] args){
         Statistics statistics = new Statistics();
         for(int i = 0; i < 10; i++){
             String nm = RandomStringUtils.random(5,"qwertyuio");
             String gen = RandomStringUtils.random(1,"fm" );
-            User user = new User(gen, nm);
+            User user = new User(gen, nm, 0);
             statistics.login(user);
 
         }
+
+        statistics.sendPoints("i",10, "f");
         System.out.println(Statistics.getMap());
-        System.out.println(Statistics.getUserSet());
+        Statistics.getUserSet()
+                .stream()
+                .filter(user -> user.getPoint() > 0)
+                .forEach(System.out::println);
+
+        OkHttpClient client = MyOkHttpClient.getClient();
+        Request request = new Request.Builder()
+                .url("https://www.lu.com")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.body().toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
 
 class User{
     private String name;
     private String gender;
+    private Integer point;
 
-    public User(String gender, String name) {
+    public User(String gender, String name, Integer point) {
         this.gender = gender;
         this.name = name;
+        this.point = point;
+
     }
 
     public String getGender() {
@@ -77,11 +123,24 @@ class User{
         this.name = name;
     }
 
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public void addPoint(Integer point){
+        this.point += point;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "gender='" + gender + '\'' +
                 ", name='" + name + '\'' +
+                ", point=" + point +
                 '}';
     }
 }
