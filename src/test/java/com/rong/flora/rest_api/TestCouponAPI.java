0551459f080c73d7a8a370d8f639fd4ae1b42487
flora <bluerwf@gmail.com>
2016-07-08 22:14:06 +0800

    Add statistics

--- src/test/java/com/rong/flora/rest_api/TestCouponAPI.java
+++ src/test/java/com/rong/flora/rest_api/TestCouponAPI.java
@@ -4,8 +4,16 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.rong.flora.practice.MyOkHttpClient;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
 
 /**
  * Created by bluerwf on 16/5/30.
@@ -54,6 +62,34 @@ public class TestCouponAPI {
         }catch (UnirestException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    @Test
+    public void testGetCoupon(){
+        String URI = host + ":" + port + uri + "/mgt/coupon";
+
+        OkHttpClient client = MyOkHttpClient.getClient();
+        Request request = new Request.Builder()
+                .url(URI)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Assert.assertEquals(response.code(), 200);
+            JSONObject body = new JSONObject(response.body().string());
+            Integer error = body.getInt("error");
+            JSONArray data = body.getJSONArray("data");
+            JSONObject coupon = data.getJSONObject(0).getJSONObject("coupon");
+            JSONArray products = data.getJSONObject(0).getJSONArray("products");
+            Integer prodCodeId = products.getJSONObject(0).getInt("prodCodeId");
+            Integer term = coupon.getInt("term");
+            System.out.println(error);
+            System.out.println(term);
+            System.out.println(products);
+            System.out.println(prodCodeId);
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
 
